677d3f3dbd029c5034b6355dfb9aea7a913b6b3a
maxwen <max.weninger@gmail.com>
2021-01-17 08:14:14 +0530

    media-caf: sm8150: add TARGET_QCOM_AUDIO_VARIANT

--- mm-video-v4l2/vidc/common/Android.mk
+++ mm-video-v4l2/vidc/common/Android.mk
@@ -21,8 +21,8 @@ libmm-vidc-def += -D_ANDROID_ICS_
 # ---------------------------------------------------------------------------------
 
 libmm-vidc-inc      := $(LOCAL_PATH)/inc
-libmm-vidc-inc      += $(call project-path-for,qcom-media)/mm-core/inc
-libmm-vidc-inc      += $(call project-path-for,qcom-media)/libc2dcolorconvert
+libmm-vidc-inc      += $(TOP)/hardware/qcom/media-caf-sm8150/mm-core/inc
+libmm-vidc-inc      += $(TOP)/hardware/qcom/media-caf-sm8150/libc2dcolorconvert
 libmm-vidc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 libmm-vidc-inc      += $(TOP)/hardware/libhardware/include
 libmm-vidc-inc      += $(LIBION_HEADER_PATHS)
